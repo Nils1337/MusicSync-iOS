61c52eeadf6c5f376dcc7a928b0af9d39f5df6e4
nils <nils@nilss-iMac.local>
2017-08-25 18:28:07 -0700

    save context

--- MusicSync/AppDelegate.swift
+++ MusicSync/AppDelegate.swift
@@ -67,6 +67,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        saveContext()
+        
         let defaults = UserDefaults.standard
         defaults.set(server?.name!, forKey: AppDelegate.serverKey)
         defaults.set(library?.id!, forKey: AppDelegate.libraryKey)
